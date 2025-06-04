79a465d8ee49b0fe2214a2a17d917c3d46690a21
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-06-04 12:39:00 +0500

    importing approutes in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-
+import { BrowserRouter } from "react-router-dom"
+import AppRoutes from "../routes"
 
 function App() {
 
   return (
     <div>
-      <h1 className="bg-blue-500 text-white p-4 text-center text-4xl">Hello World</h1>
+      <BrowserRouter><AppRoutes/></BrowserRouter>
+     
     </div>
   )
 }
