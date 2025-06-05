513263af35359f083203643a891b8f6d03cced6c
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-06-06 01:02:37 +0500

    importing authserice as default in services types

--- src/services/services.ts
+++ src/services/services.ts
@@ -1,5 +1,5 @@
-export * from "./authServices";
-// export * from "./followServices";
-// export * from "./postsServices";
-// export * from "./profileServices";
-// export * from "./storyServices";
+export * as AuthService from "./authServices";
+// export * as FollowServices from "./followServices";
+// export * as PostsServices from "./postsServices";
+// export * as ProfileServices from "./profileServices";
+// export * as StoryServices from "./storyServices";
