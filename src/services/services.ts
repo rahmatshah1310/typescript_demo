2b9b183eda8110c9cdb71e90051fa67e10bf8aaa
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-06-05 12:33:00 +0500

    commenting all the export route remain one

--- src/services/services.ts
+++ src/services/services.ts
@@ -1,5 +1,5 @@
 export * from "./authServices";
-export * from "./followServices";
-export * from "./postsServices";
-export * from "./profileServices";
-export * from "./storyServices";
+// export * from "./followServices";
+// export * from "./postsServices";
+// export * from "./profileServices";
+// export * from "./storyServices";
