aeb85274bb91cef8d57308a4e8ca8718c6bd2203
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-06-05 12:29:25 +0500

    adding queryclient in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import { BrowserRouter } from "react-router-dom"
 import AppRoutes from "../routes"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 60 * 5, // 5 minutes
+        gcTime: 1000 * 60 * 10, // 10 minutes (renamed from cacheTime in v5)
+      },
+    },
+  });
 
   return (
-    <div>
-      <BrowserRouter><AppRoutes/></BrowserRouter>
-     
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
 
