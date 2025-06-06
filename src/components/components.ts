21da14f4edb12af1e7ac39574deed82082ede42a
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-06-07 00:43:36 +0500

    export spinner in component

--- src/components/components.ts
+++ src/components/components.ts
@@ -4,6 +4,7 @@
 // export * from "./comments/CommentLikesFooter";
 export {default as Button} from "./common/Button";
 export {default as ErrorMessage} from "./common/ErrorMessage";
+export {default as Spinner} from "./common/Spinner";
 // export * from "./common/Modal";
 // export * from "./common/Spinner";
 // export * from "./common/LoadingBar";
