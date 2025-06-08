97ab700d963627564e2395a977cb67aec09b5134
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-06-08 16:48:14 +0500

    uncommeting dialog component

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,22 +1,21 @@
-// import {
-//   Dialog,
-//   DialogTrigger,
-//   DialogContent,
-//   DialogHeader,
-//   DialogTitle,
-//   DialogDescription,
-//   DialogClose,
-// } from "@/components/ui/dialog";
-// import Button from "@components/common/Button";
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+  DialogClose,
+} from "@/components/ui/dialog";
 
-// const Modal = ({ children, isOpen, onClose, className }) => {
-//   return (
-//     <Dialog open={isOpen} onOpenChange={onClose}>
-//       <DialogContent className={`${className}`}>
-//         <div>{children}</div>
-//       </DialogContent>
-//     </Dialog>
-//   );
-// };
+const Modal = ({ children, isOpen, onClose, className }) => {
+  return (
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent className={`${className}`}>
+        <div>{children}</div>
+      </DialogContent>
+    </Dialog>
+  );
+};
 
-// export default Modal;
+export default Modal;
