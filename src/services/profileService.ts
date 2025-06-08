1bb80c438093b082809348e77e3045a993b40fd4
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-06-08 16:53:57 +0500

    feat: adding addprofilepic function as sending post request also use updateprofile function for upating the profile

---
+++ src/services/profileService.ts
@@ -0,0 +1,35 @@
+import { sendRequest } from "@utils";
+
+export const addProfilePic = async (data: FormData) => {
+  try {
+    const response = await sendRequest({
+      method: "POST",
+      url: "/profile-settings/profile-pic",
+      data,
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.log(`Profile Service [addProfilePic] error: ${error}`);
+    throw error;
+  }
+};
+
+export const updateProfile = async (data: FormData) => {
+  try {
+    const response = await sendRequest({
+      method: "PUT",
+      url: "/profile-settings/profile-pic",
+      data,
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.log(`Profile Service [updateProfile] error: ${error}`);
+    throw error;
+  }
+};
