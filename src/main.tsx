0ec2b0c1402a8e51be4a159b7f7e09c45e5c7868
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-06-09 20:16:16 +0500

    feat:adding react-query-devtool,also adding queryclient funccton for showing devtool on the screen and then render it

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,22 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>
 )
