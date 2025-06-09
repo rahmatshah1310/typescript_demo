669612bcbbdef7bdffbff81c5f93c780bec357a6
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-06-09 16:42:27 +0500

    feat:adding shadcenpopver component to the components folder

--- src/components/common/ShadcnPopover.tsx
+++ src/components/common/ShadcnPopover.tsx
@@ -1,24 +1,29 @@
-// import {
-//   Popover,
-//   PopoverContent,
-//   PopoverTrigger,
-// } from "@/components/ui/popover";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { ReactNode, ReactElement } from "react";
 
-// const ShadcnPopover = ({ triggerContent, children }) => {
-//   return (
-//     <Popover>
-//       <PopoverTrigger asChild>{triggerContent}</PopoverTrigger>
-//       <PopoverContent
-//         align="start"
-//         side="top"
-//         sideOffset={8}
-//         className="w-64 bg-gray-700 text-black border border-gray-300 shadow-lg z-[900]"
-//       >
-//         {children}
-//       </PopoverContent>
-//     </Popover>
-//   );
-// };
+interface ShadcnPopoverProps {
+  triggerContent: ReactElement;
+  children: ReactNode;
+}
 
-// export default ShadcnPopover;
-// // 
+const ShadcnPopover: React.FC<ShadcnPopoverProps> = ({ triggerContent, children }) => {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>{triggerContent}</PopoverTrigger>
+      <PopoverContent
+        align="start"
+        side="top"
+        sideOffset={8}
+        className="w-64 bg-gray-700 text-black border border-gray-300 shadow-lg z-[900]"
+      >
+        {children}
+      </PopoverContent>
+    </Popover>
+  );
+};
+
+export default ShadcnPopover;
