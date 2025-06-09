686598bcf7402f3780569a0f10b77509285513f6
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-06-09 16:59:57 +0500

    props:adding postservice to the services.ts file

--- src/services/services.ts
+++ src/services/services.ts
@@ -1,6 +1,6 @@
 export * as AuthService from "./authServices";
 export * as ProfileService from "./profileService";
 // export * as FollowServices from "./followServices";
-// export * as PostsServices from "./postsServices";
+export * as PostService from "./postService";
 // export * as ProfileServices from "./profileServices";
 // export * as StoryServices from "./storyServices";
