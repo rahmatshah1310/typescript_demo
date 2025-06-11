31047e5b1c15f78d0bb492583f7df6098ebf9b3a
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-06-11 22:56:28 +0500

    feat:use queryclient invalidator for real time updating data also add onsuccess and onerror

--- src/api/profileApi.ts
+++ src/api/profileApi.ts
@@ -1,10 +1,18 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation,useQueryClient } from "@tanstack/react-query";
 import {ProfileService} from "@services";
 
 export const useAddProfilePicMutation = () => {
+  const queryClient=useQueryClient()
   return useMutation({
     mutationFn: async (formData: FormData) => {
       return await ProfileService.addProfilePic(formData);
+    },
+    onSuccess: () => {
+      alert("Post created successfully!");
+      queryClient.invalidateQueries({ queryKey: ["profile"] });
+    },
+    onError: (error: any) => {
+      alert(`Post creation failed: ${error?.message || "Unknown error"}`);
     },
   });
 };
