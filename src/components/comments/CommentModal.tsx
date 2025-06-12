1d5d86d1b584b9963499f9ffbb7a50536002812d
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-06-12 22:41:55 +0500

    feat:adding commentmodal component and pass isopen,onclose and ondelete as a props

--- src/components/comments/CommentModal.tsx
+++ src/components/comments/CommentModal.tsx
@@ -1,21 +1,20 @@
-// // components/CommentModal.jsx
-// import React from "react";
-// import Modal from "@/components/common/Modal";
-// import Button from "@/components/common/Button";
+// components/CommentModal.jsx
+import React from "react";
+import {Modal,Button} from "@components";
 
-// const CommentModal = ({ isOpen, onClose, onDelete }) => {
-//   return (
-//     <Modal isOpen={isOpen} onClose={onClose}>
-//       <div className="bg-[#262626] p-4 rounded-md text-white w-96  flex flex-col space-y-4">
-//         <Button className="text-red-500" onClick={onDelete}>
-//           Delete
-//         </Button>
-//         <Button className="text-gray-300" onClick={onClose}>
-//           Cancel
-//         </Button>
-//       </div>
-//     </Modal>
-//   );
-// };
+const CommentModal = ({ isOpen, onClose, onDelete }) => {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <div className="bg-[#262626] p-4 rounded-md text-white w-96  flex flex-col space-y-4">
+        <Button className="text-red-500" onClick={onDelete}>
+          Delete
+        </Button>
+        <Button className="text-gray-300" onClick={onClose}>
+          Cancel
+        </Button>
+      </div>
+    </Modal>
+  );
+};
 
-// export default CommentModal;
+export default CommentModal;
