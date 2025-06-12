e567945cd66f219e7bb70a52c12067b6364aa6bf
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-06-12 22:38:37 +0500

    props:adding routes for postdetails , Also adding toasify to the app

--- routes.tsx
+++ routes.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { ROUTES } from "@constants";
-import { SignUp ,Login} from "@pages";
+import { SignUp, Login } from "@pages";
 import { Routes as RouterRoutes, Route } from "react-router-dom";
-import {AuthLayout,AppLayout} from "@layouts";
-import {Home,Profile} from "@pages";
+import { AuthLayout, AppLayout } from "@layouts";
+import { Home, Profile } from "@pages";
+import { ToastContainer } from "react-toastify";
+import PostDetails from "./src/pages/Profile/components/PostDetails";
 
 const AppRoutes: React.FC = () => {
   return (
-    <RouterRoutes>
+    <> <RouterRoutes>
       <Route path={ROUTES.auth} element={<AuthLayout />}>
         <Route path="signup" element={<SignUp />} />
         <Route path="login" element={<Login />} />
@@ -15,9 +17,18 @@ const AppRoutes: React.FC = () => {
 
       <Route path="/" element={<AppLayout />}>
         <Route index element={<Home />} />
-          <Route path=":username" element={<Profile />} />
+        <Route path=":username" element={<Profile />} />
+        <Route path="/p/:postId" element={<PostDetails isOpen />} />
       </Route>
     </RouterRoutes>
+
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        pauseOnHover
+        draggable
+        theme="colored"
+      /></>
   );
 };
 
