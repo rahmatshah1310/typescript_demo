a417a9774fe7efc7949f992a5504ab7c0bd8d1a3
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-06-12 22:40:20 +0500

    props:adding routes for commentmodal ,commentlikesfooter and also for tab

--- src/components/components.ts
+++ src/components/components.ts
@@ -1,24 +1,21 @@
 // export * from "./chat/ChatInput";
 // export * from "./chat/ChatContainer";
-// export * from "./comments/CommentModal";
-// export * from "./comments/CommentLikesFooter";
+export {default as CommentModal} from "./comments/CommentModal";
+export {default as CommentLikesFooter} from "./comments/CommentLiskesFooter";
 export {default as Button} from "./common/Button";
 export {default as ErrorMessage} from "./common/ErrorMessage";
 export {default as Spinner} from "./common/Spinner";
 export {default as Sidebar} from "./layout/Sidebar"
 export {default as Header} from "./layout/Header"
 export {default as Modal} from "./common/Modal";
-// export * from "./common/Spinner";
 // export * from "./common/LoadingBar";
 export {default as ShadcnPopover} from "./common/ShadcnPopover";
 export {default as SearchComponent} from "./common/SearchComponent";
 export {default as InputField} from "./common/InputField";
 export {default as AppFooter} from "./layout/Footer";
 export * from "./skeletons/PostSkeleton"
-// export * from "./layout/Header";
-// export * from "./layout/Sidebar";   
 // export * from "./posts/PostHeader";
-// export * from "./tabs/Tabs";
+export {default as Tab} from "./tabs/Tab";
 // export * from "./ui/dialog";
 export * from "./ui/skeleton";
 export * from "./ui/popover";
