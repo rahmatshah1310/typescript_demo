97238ff3c655efcbebd9f567592e83f065c830d7
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-06-14 12:12:03 +0500

    feat:Adding index.ts for constants,context,hooks,layouts and pages

---
+++ src/pages/index.ts
@@ -0,0 +1,12 @@
+// pages/index.ts
+// export { default as Home } from "./Home/Home";
+export { default as Profile } from "./Profile/Profile";
+// export { default as Explore } from "./Explore/Explore";
+export { default as Login } from "./Login";
+export { default as SignUp } from "./Signup";
+export {default as Home} from "./Home/Home"
+// export { default as Notifications } from "./Notification";
+export { default as Create } from "./Create/Create";
+// export { default as Messages } from "./messages/Messages";
+// export { default as NotFound } from "./NotFound";
+
