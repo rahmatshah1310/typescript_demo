a5cf31ae791b19402a1a6f0f23536bccf67fda94
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-06-15 05:14:22 +0500

    feat:Adding _filenameurltopath to remove the type error of dirname

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'node:path';
+import {fileURLToPath} from 'node:url'
+
+const _filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(_filename)
 
 // https://vite.dev/config/
 export default defineConfig({
