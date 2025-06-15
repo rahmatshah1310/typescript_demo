3c19b35643c5add19665707b9b6e99607afa8e55
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-06-15 05:16:55 +0500

    feat:adding postheaderprops and user props for typescript also adding user and its components

--- src/components/posts/PostHeader.tsx
+++ src/components/posts/PostHeader.tsx
@@ -1,23 +1,34 @@
-// import React from "react";
-// import Button from "@/components/common/Button";
-// import { ICONS } from "@/assets/icons";
+import React from "react";
+import Button from "@components";
+import { ICONS } from "@/assets/icons";
 
-// const PostHeader = ({ user, onOptionClick, className = "" }) => {
-//   return (
-//     <div className={className}>
-//       <div className="flex items-center gap-4">
-//         <img
-//           src={user?.profilePic}
-//           alt="profilePic"
-//           className="w-8 h-8 rounded-full object-cover"
-//         />
-//         <h2 className="text-white text-xs">{user?.username}</h2>
-//       </div>
-//       <Button className="text-white" onClick={onOptionClick}>
-//         {ICONS.threeDots}
-//       </Button>
-//     </div>
-//   );
-// };
+interface User {
+  profilePic: string;
+  username: string;
+}
 
-// export default PostHeader;
+interface PostHeaderProps {
+  user: User | null;
+  onOptionClick: () => void;
+  className?: string;
+}
+
+const PostHeader: React.FC<PostHeaderProps> = ({ user, onOptionClick, className = "" }) => {
+  return (
+    <div className={className}>
+      <div className="flex items-center gap-4">
+        <img
+          src={user?.profilePic}
+          alt="profilePic"
+          className="w-8 h-8 rounded-full object-cover"
+        />
+        <h2 className="text-white text-xs">{user?.username}</h2>
+      </div>
+      <Button className="text-white" onClick={onOptionClick}>
+        {ICONS.threeDots}
+      </Button>
+    </div>
+  );
+};
+
+export default PostHeader;
