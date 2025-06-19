d0ea4c724748e0d67d9770d5570b8689077949c4
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-06-19 23:47:30 +0500

    feat:Adding function for adding comment on post also use usemutataion for getting comments

--- src/components/comments/CommentLiskesFooter.tsx
+++ src/components/comments/CommentLiskesFooter.tsx
@@ -1,29 +1,54 @@
 import React, { useEffect, useRef, useState } from "react";
 import { ICONS } from "@constants";
-import {Modal,Button,Skeleton,PostSkeleton} from "@components";
+import { Modal, Button, Skeleton, PostSkeleton } from "@components";
 import { useAuth } from "@context";
-import type { Post } from "../../types/profile";
+import { useCreateCommentMutation } from "@api";
+import { toast } from "react-toastify";
+import { useGetAllComments } from "../../api";
 
 interface CommentLikesFooterProps {
-  post: Post[]; 
+  post: Post[];
   replyTo: string | null;
   setReplyTo: (value: string | null) => void;
 }
 
-const CommentLikesFooter: React.FC<CommentLikesFooterProps> = ({ post, replyTo, setReplyTo }) => {
+const CommentLikesFooter: React.FC<CommentLikesFooterProps> = ({ post, postId, replyTo, setReplyTo }) => {
   const { userData } = useAuth();
   const inputRef = useRef();
   const [isLiked, setIsLiked] = useState(false);
   const [likeCount, setLikeCount] = useState(0);
-//   const [comment, setComment] = useState([]);
+  //   const [comment, setComment] = useState([]);
   const [commentText, setCommentText] = useState("");
-//   const [loadingComments, setLoadingComments] = useState(false);
-//   const [loadingLikes, setLoadingLikes] = useState(false);
+  //   const [loadingComments, setLoadingComments] = useState(false);
+  //   const [loadingLikes, setLoadingLikes] = useState(false);
+  const commentMutation = useCreateCommentMutation();
+  const { data:comments , error, isSuccess, isError } = useGetAllComments(postId);
+console.log(comments,"this is all comments")
 
-  // <---------------------------------------- Loading if Post is not Available ----------------------------------------->
-  if (!post) {
-    return <div className="text-white">No Post Available.</div>;
-  }
+  const handleComment = () => {
+    if (!postId || !commentText.trim()) {
+      toast.error("Missing post ID, or comment text");
+      return;
+    }
+    const formData = new FormData();
+    formData.append("comment", commentText)
+    commentMutation.mutate(
+      {
+        postId, formData
+      },
+    );
+  };
+
+  // <---------------------------------------- Loading if xPost is not Available ----------------------------------------->
+  useEffect(() => {
+    if (commentMutation.status === "success") {
+      toast.success(commentMutation.data?.message || "Comment Added successfully!");
+    } else if (commentMutation.status === "error") {
+      const errorMessage = commentMutation.error as any;
+      toast.error(`Failed to create comment!\n${errorMessage?.message || "Unknown error"}`);
+    }
+  }, [commentMutation.status, commentMutation]);
+
 
   return (
     <div className="bg-black border-t border-gray-700 p-4">
@@ -32,9 +57,8 @@ const CommentLikesFooter: React.FC<CommentLikesFooterProps> = ({ post, replyTo, 
         <div className="flex items-center gap-4">
           <Button
             // onClick={handleLike}
-            className={`text-gray-400 ${
-              isLiked ? "text-red-500 hover:text-red-600" : ""
-            }`}
+            className={`text-gray-400 ${isLiked ? "text-red-500 hover:text-red-600" : ""
+              }`}
           >
             {isLiked ? ICONS.heartFilled : ICONS.ciHeart}
           </Button>
@@ -75,7 +99,7 @@ const CommentLikesFooter: React.FC<CommentLikesFooterProps> = ({ post, replyTo, 
         />
         <Button
           className="text-blue-500 font-bold"
-        //   onClick={handleComment}
+          onClick={handleComment}
           disabled={!commentText.trim()}
         >
           Post
