28f04d539eb288904c31af11797035c393e4327c
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-06-19 23:46:15 +0500

    feat:Added mutation for creating comment on post , Also create mutation for deleting comment, and add queryfunction for getting all posts

---
+++ src/api/commentApi.ts
@@ -0,0 +1,26 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { CommentService } from "@services";
+
+// ✅ Create Comment Mutation
+export const useCreateCommentMutation = () => {
+  return useMutation({
+    mutationFn: ({ postId, formData }: { postId: string; formData: FormData }) =>
+      CommentService.addComment(postId, formData),
+  });
+};
+
+// ✅ Get All Comments for a specific post
+export const useGetAllComments = (postId: string) => {
+  return useQuery({
+    queryKey: ["comments", postId], 
+    queryFn: () => CommentService.getAllComment(postId),
+    enabled: !!postId, 
+  });
+};
+
+// ✅ Delete Comment Mutation
+export const useDeleteCommentMutation = () => {
+  return useMutation({
+    mutationFn: (commentId: string) => CommentService.deleteComment(commentId),
+  });
+};
