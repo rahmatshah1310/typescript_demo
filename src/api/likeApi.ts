cef636bc06648a49e9ffff900cb7a3aca979c332
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-06-22 19:39:42 +0500

    feat:Add uselikemutation also add mutation for dislike post

--- src/api/likeApi.ts
+++ src/api/likeApi.ts
@@ -1,2 +1,17 @@
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import { LikeService } from "@services";
+
+export const useLikeMutation = () => {
+  return useMutation({
+    mutationFn: ({ postId }: { postId: string}) =>
+      LikeService.likePost(postId),
+  });
+};
+
+
+export const useDislikeMutation = () => {
+  return useMutation({
+    mutationFn: ({ postId }: { postId: string}) =>
+      LikeService.dislikePost(postId),
+  });
+};
