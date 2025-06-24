b35e927dcfa21998128c39eaa58de4b51669e387
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-06-24 22:44:08 +0500

    feat:Adding inteface for commentprops

--- src/components/comments/CommentLiskesFooter.tsx
+++ src/components/comments/CommentLiskesFooter.tsx
@@ -5,6 +5,7 @@ import { useAuth } from "@context";
 import { useCreateCommentMutation,useDislikeMutation,useLikeMutation } from "@api";
 import { toast } from "react-toastify";
 
+// inteface for commentprops
 interface CommentLikesFooterProps {
   post: [];
   replyTo: string | null;
