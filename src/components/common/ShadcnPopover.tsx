11af8f101122961f4ff885dfa568caee1b9fc55b
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-06-25 19:57:45 +0500

    Removing extra comments

--- src/components/common/ShadcnPopover.tsx
+++ src/components/common/ShadcnPopover.tsx
@@ -5,7 +5,6 @@ import {
 } from "@/components/ui/popover";
 import { ReactNode, ReactElement } from "react";
 
-// interface for shdcnprops
 interface ShadcnPopoverProps {
   triggerContent: ReactElement;
   children: ReactNode;
