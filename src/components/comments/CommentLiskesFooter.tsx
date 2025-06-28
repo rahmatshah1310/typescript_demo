3ff5ffbf7bcfec74e8442ef4ec813594c6b071ac
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-06-28 20:33:29 +0500

    fix:interface for commlikesfooter

--- src/components/comments/CommentLiskesFooter.tsx
+++ src/components/comments/CommentLiskesFooter.tsx
@@ -5,6 +5,7 @@ import { useAuth } from "@context";
 import { useCreateCommentMutation,useDislikeMutation,useLikeMutation } from "@api";
 import { toast } from "react-toastify";
 
+//interface
 interface CommentLikesFooterProps {
   post: [];
   replyTo: string | null;
