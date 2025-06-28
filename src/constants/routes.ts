38c14a7e63808dbf1af4d16abb71ff11314743dc
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-06-28 20:36:12 +0500

    removing extra comments

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,7 +1,7 @@
 export const ROUTES = {
     auth: "/auth",
     login: "/auth/login",    // Updated to include parent path
-    signup: "/auth/signup",  // Updated to include parent path
+    signup: "/auth/signup", 
     home: "/",              
    profile: (username) => `/${username}`,
     search: "/search",
