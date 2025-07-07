28888c7fca25bfeb92a5f0011dc4c962dac96ca8
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-07-07 16:20:08 +0500

    removing extra comments

--- src/constants/fonts.ts
+++ src/constants/fonts.ts
@@ -2,7 +2,7 @@ export const FONTS = {
   primary: 'Roboto, Arial, sans-serif', // Instagram primary font
   secondary: 'Helvetica, Arial, sans-serif', // Instagram secondary font
   bold: 'Roboto-Bold, Arial-Bold, sans-serif', // Bold font
-  light: 'Roboto-Light, Arial-Light, sans-serif', // Light font
+  light: 'Roboto-Light, Arial-Light, sans-serif', 
   medium: 'Roboto-Medium, Arial-Medium, sans-serif', 
   italic: 'Roboto-Italic, Arial-Italic, sans-serif', 
 };
