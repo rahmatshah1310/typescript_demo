5c3de430bf1d82078c98ccd1e40485161959af80
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-07-31 22:53:45 +0500

    remove

--- src/api/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./authApi";
-export * from "./followApi";
-export * from "./commentApi"
-export * from "./postApi";
-export * from "./profileApi";
-export * from "./storyApi";
-export * from "./likeApi";
