1a636a9c77c219d28b1ec56c61bed3a94edd9f8f
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-07-31 22:52:50 +0500

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { BrowserRouter } from "react-router-dom"
-import AppRoutes from "../routes"
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "../routes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { AuthProvider } from "@context";
+// import { AuthProvider } from "@context";
 
 const App: React.FC = () => {
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
         staleTime: 1000 * 60 * 5, // 5 minutes
-        gcTime: 1000 * 60 * 10, 
+        gcTime: 1000 * 60 * 10,
       },
     },
   });
@@ -16,12 +16,12 @@ const App: React.FC = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <AuthProvider>
-          <AppRoutes /> 
-        </AuthProvider>
+        {/* <AuthProvider> */}
+        <AppRoutes />
+        {/* </AuthProvider> */}
       </BrowserRouter>
     </QueryClientProvider>
   );
 };
 
-export default App
+export default App;
