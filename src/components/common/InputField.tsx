b5e33cd0b3e60734e3701214702d6d3ea21e5740
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-08-01 21:46:07 +0500

    add

--- src/components/common/InputField.tsx
+++ src/components/common/InputField.tsx
@@ -1,90 +1,35 @@
-import {
-  useState,
-  type InputHTMLAttributes,
-} from "react";
+import { forwardRef, InputHTMLAttributes } from "react";
 
-import type { UseFormRegisterReturn } from "react-hook-form";
-import type { IconType } from "react-icons";
-import {ErrorMessage,Button} from "@components";
-
-//inputprops interface
-interface InputFieldProps
-  extends InputHTMLAttributes<HTMLInputElement> {
-  label?: string;
-  inputClassname?: string;
-  name?: string;
-  icon?: IconType;
-  size?: number;
-  color?: string;
-  error?: string;
-  register?: UseFormRegisterReturn;
-  isPassword?: boolean;
+interface InputFieldProps extends InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  className?: string;
 }
 
-const InputField: React.FC<InputFieldProps> = (
-  (
-    {
-      label,
-      placeholder,
-      inputClassname,
-      name,
-      type = "text",
-      icon: Icon,
-      size = 18,
-      color = "#555",
-      error,
-      register,
-      isPassword = false,
-      ...rest
-    },
-  ) => {
-    const [selectedType, setSelectedType] = useState<string>(type);
+const InputField = forwardRef<HTMLInputElement, InputFieldProps>(({ type, id, name, className = "", label, ...rest }, ref) => {
+  return (
+    <div className="relative">
+      <input
+        type={type}
+        id={id}
+        name={name}
+        ref={ref}
+        {...rest}
+        className={`peer block py-2 w-full appearance-none border bg-transparent px-2.5 text-sm text-white border-[#555555] focus:outline-none focus:ring-0 focus:border-[#555555] ${className}`}
+        placeholder=" "
+      />
+      <label
+        htmlFor={id}
+        className="absolute top-2 left-2.5 z-10 origin-[0] transform text-base text-[#F5F5F5] transition-all duration-300
+            peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100
+            peer-focus:-translate-y-3 peer-focus:scale-75
+            peer-not-placeholder-shown:-translate-y-3 peer-not-placeholder-shown:scale-75"
+      >
+        {label}
+      </label>
+    </div>
+  );
+});
 
-    return (
-      <div className="space-y-2">
-        <div className="relative w-full">
-          <input
-            {...register}
-            {...rest}
-            type={selectedType}
-            id={name}
-            name={name}
-            placeholder={placeholder ?? ""}
-            className={`peer block py-2 w-full appearance-none border bg-transparent text-sm text-white border-[#555555] focus:outline-none focus:ring-0 focus:border-[#555555] ${inputClassname}`}
-          />
-          {Icon && (
-            <Icon
-              className="absolute top-1/2 -translate-y-1/2 left-3"
-              size={size}
-              color={color}
-            />
-          )}
-          {isPassword && (
-            <Button
-              onClick={() =>
-                setSelectedType(
-                  selectedType === "password" ? "text" : "password"
-                )
-              }
-              className="absolute top-1/2 -translate-y-1/2 right-3"
-            >
-              {selectedType === "password" ? "Show" : "Hide"}
-            </Button>
-          )}
-          <label
-            htmlFor={name}
-            className="absolute top-2 left-2.5 z-10 origin-[0] transform text-base text-[#F5F5F5] transition-all duration-300
-          peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100
-          peer-focus:-translate-y-3 peer-focus:scale-75
-          peer-not-placeholder-shown:-translate-y-3 peer-not-placeholder-shown:scale-75"
-          >
-            {label}
-          </label>
-        </div>
-        {error && <ErrorMessage error={error} />}
-      </div>
-    );
-  }
-);
+InputField.displayName = "InputField"; // Required for forwardRef with named components
 
 export default InputField;
