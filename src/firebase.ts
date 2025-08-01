111ae9b28364ffd2bf7d8ddf952db1f07628f0ab
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-08-01 21:42:28 +0500

    add

---
+++ src/firebase.ts
@@ -0,0 +1,25 @@
+// src/firebase.ts
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+// Vite exposes environment variables with import.meta.env
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Firebase services
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export default app;
