cdfea3650621908e8c503af520f6b119d0d90a85
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-08-01 21:49:15 +0500

    add

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -1,15 +1,15 @@
-import { ICONS } from "@/constants/iconsMap";
+import { ICONS } from "./icons";
 
 export const tabs = [
   {
     id: "posts",
     label: "POSTS",
-    icon: ICONS.mdGridOn,
+    // icon: ICONS.mdGridOn,
   },
   {
     id: "saved",
     label: "SAVED",
-    icon: ICONS.bookMark,
+    // icon: ICONS.bookMark,
   },
   {
     id: "tagged",
@@ -22,22 +22,10 @@ export const COLOR = {
   primaryColor: "#1877F2",
 };
 
-export const MORE_MENU_ITEMS = [
-  "Settings",
-  "Your activity",
-  "Saved",
-  "Switch appearance",
-  "Report a problem",
-  "Switch accounts",
-];
+export const MORE_MENU_ITEMS = ["Settings", "Your activity", "Saved", "Switch appearance", "Report a problem", "Switch accounts"];
 
 export const PANEL_SECTIONS = ["search", "notifications", "messages"];
 
+export const isValidPanelSection = (section) => PANEL_SECTIONS.includes(section);
 
-export const isValidPanelSection = (section) =>
-  PANEL_SECTIONS.includes(section);
-
-export const isValidActiveSection = (section) =>
-  [...PANEL_SECTIONS, "create", "more", null].includes(section);
-
-//new
+export const isValidActiveSection = (section) => [...PANEL_SECTIONS, "create", "more", null].includes(section);
