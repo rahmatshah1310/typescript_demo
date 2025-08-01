d1cd7b1e88e49bb421481d5963e0e6c55e607c53
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-08-01 21:41:56 +0500

    add

--- routes.tsx
+++ routes.tsx
@@ -1,10 +1,10 @@
-import React from "react";
 import { ROUTES } from "@constants";
-import { Login, SignUp } from "@pages";
+import { Login, Profile, SignUp } from "@pages";
 import { Routes as RouterRoutes, Route } from "react-router-dom";
 // import { AuthLayout, AppLayout } from "@layouts";
 // import { Home, Profile } from "@pages";
 import { ToastContainer } from "react-toastify";
+import { AppLayout } from "@layouts";
 // import SignUp from "src/pages/Signup";
 // import Login from "src/pages/Login";
 // import PostDetails from "./src/pages/Profile/components/PostDetails";
@@ -12,18 +12,17 @@ import { ToastContainer } from "react-toastify";
 const AppRoutes: React.FC = () => {
   return (
     <>
-      {" "}
       <RouterRoutes>
         <Route path={ROUTES.auth}>
           <Route path="signup" element={<SignUp />} />
           <Route path="login" element={<Login />} />
         </Route>
 
-        {/* <Route path="/" element={<AppLayout />}>
-        <Route index element={<Home />} />
-        <Route path=":username" element={<Profile />} />
-        {/* <Route path="p/:postId" element={<PostDetails isOpen />} /> */}
-        {/* </Route> */}
+        <Route path="/" element={<AppLayout />}>
+          {/* <Route index element={<Home />} /> */}
+          <Route path=":username" element={<Profile />} />
+          {/* <Route path="p/:postId" element={<PostDetails isOpen />} /> */}
+        </Route>
       </RouterRoutes>
       <ToastContainer position="top-right" autoClose={3000} pauseOnHover draggable theme="colored" />
     </>
