2cc67410b2dc24025a9d65ff431eb7a1d02a4812
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-08-02 17:17:00 +0500

    add

---
+++ src/hooks/useAuthContext.ts
@@ -0,0 +1,11 @@
+// import { useContext } from "react";
+// import { AuthContext, AuthContextType } from "../context/AuthContext";
+
+// export const useAuthContext = (): AuthContextType => {
+//   const context = useContext(AuthContext);
+//   if (!context) {
+//     throw new Error("useAuthContext must be used within an AuthProvider");
+//   }
+//   return context;
+// };
+// //
