82d333cf445c2c3870cb89638dce145de844dcfe
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-08-03 19:18:55 +0500

    feat(CreatePost): add CreatePost component with modal for post creation

---
+++ src/components/postcomponents/CreatePost.tsx
@@ -0,0 +1,17 @@
+import { PostComponent, Modal } from "@components";
+
+const CreatePost = ({ isOpen, onClose }) => {
+  return (
+    <>
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose} // handles outside click
+        className="w-[90%] sm:w-[500px] md:w-[600px] lg:w-[700px] xl:w-[800px] 2xl:w-[900px]"
+      >
+        <PostComponent isOpen={isOpen} onClose={onClose} />
+      </Modal>
+    </>
+  );
+};
+
+export default CreatePost;
