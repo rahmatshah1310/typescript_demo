de9a59c36be355a0f3a88b54634d1cb61622e144
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-08-04 19:22:05 +0500

    add

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,7 @@
 export { default as SignUp } from "./Signup";
 export { default as Login } from "./Login";
+export { default as ForgotPassword } from "./ForgotPassword";
+export { default as ResetPassword } from "./ResetPassword";
 export { default as Profile } from "./Profile";
 export { default as Home } from "./Home";
+export { default as Explore } from "./Explore";
