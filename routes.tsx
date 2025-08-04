5ad3cb47c389a4c86c9a3fcb6cd1ef4b421dd440
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-08-04 19:19:45 +0500

    add

--- routes.tsx
+++ routes.tsx
@@ -1,21 +1,24 @@
 import { ROUTES } from "@constants";
-import { Home, Login, Profile, SignUp } from "@pages";
+import { Home, Login, Profile, SignUp, Explore, ForgotPassword, ResetPassword } from "@pages";
 import { Routes as RouterRoutes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import { AppLayout } from "@layouts";
+import { AppLayout, AuthLayout } from "@layouts";
 import { PostDetails } from "@components";
 
 const AppRoutes: React.FC = () => {
   return (
     <>
       <RouterRoutes>
-        <Route path={ROUTES.auth}>
+        <Route path={ROUTES.auth} element={<AuthLayout />}>
           <Route path="signup" element={<SignUp />} />
           <Route path="login" element={<Login />} />
+          <Route path="forgot-password" element={<ForgotPassword />} />
+          <Route path="reset-password" element={<ResetPassword />} />
         </Route>
 
         <Route path="/" element={<AppLayout />}>
           <Route index element={<Home />} />
+          <Route path="explore" element={<Explore />} />
           <Route path=":username" element={<Profile />} />
           <Route path="p/:postId" element={<PostDetails />} />
         </Route>
