700f5c9dbb3f9df81dbfd00bfff64d5352d6ca5c
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-08-04 19:19:57 +0500

    add

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -1,4 +1,4 @@
-import { loginUser, signupUser } from "@services";
+import { forgotPassword, loginUser, logoutUser, resetPassword, signupUser } from "@services";
 import { useMutation } from "@tanstack/react-query";
 
 export const useSignup = () => {
@@ -14,5 +14,23 @@ export const useLogin = () => {
     mutationFn: ({ email, password }: { email: string; password: string }) => {
       return loginUser(email, password);
     },
+  });
+};
+
+export const useForgotPasswordMutation = () => {
+  return useMutation({
+    mutationFn: forgotPassword,
+  });
+};
+
+export const useResetPasswordMutation = () => {
+  return useMutation({
+    mutationFn: resetPassword,
+  });
+};
+
+export const useLogout = () => {
+  return useMutation({
+    mutationFn: logoutUser,
   });
 };
