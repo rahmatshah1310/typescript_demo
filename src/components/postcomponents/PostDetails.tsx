5a77f26e721a741c95f53b3e666da22a9003d774
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-08-05 23:17:55 +0500

    add

--- src/components/postcomponents/PostDetails.tsx
+++ src/components/postcomponents/PostDetails.tsx
@@ -1,24 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { ICONS } from "@constants";
 import { Button, Modal, Spinner, PostHeader, PostOptionsModal, CommentModal, CommentLikesFooter } from "@components";
-// import { usePost } from "@/features/context/PostContext";
-// import PostOptionsModal from "@pages/Profile/components/PostOptionsModal";
 import { getShortTimeAgo } from "@utils";
-import { useComments, useGetPostById } from "@api";
+import { useComments, useDeleteCommentMutation, useGetPostById } from "@api";
 import { CommentSkeleton } from "../skeletons/PostSkeleton";
-// import CommentLikesFooter from "@/components/comments/CommentLiskesFooter";
-// import PostHeader from "@/components/header/PostHeader";
+import { toast } from "react-toastify";
 
 type PostDetailsProps = {
   isOpen: boolean;
   onClose: () => void;
   user: User | null;
+  imageUrl: string | null;
+  showCommentButton: boolean;
   showDeleteButton: boolean;
   post: Post | null;
 };
 
 const PostDetails: React.FC<PostDetailsProps> = ({ isOpen, onClose, post }) => {
-  // const { deleteComment, addLikeForComment, removeLikeForComment } = usePost();
   // const [comment, setComment] = useState([]);
   // const [loadingComments, setLoadingComments] = useState(false);
   const [isOptionsModalOpen, setIsOptionsModalOpen] = useState(false);
@@ -26,11 +24,33 @@ const PostDetails: React.FC<PostDetailsProps> = ({ isOpen, onClose, post }) => {
 
   const { data: singlePost, isLoading: loadingPost } = useGetPostById(post?.id);
   const { data: comments = [], isLoading: loadingComments } = useComments(post?.id);
-  // const [selectedCommentId, setSelectedCommentId] = useState(null);
+  const deleteCommentMutation = useDeleteCommentMutation();
+  const [selectedCommentId, setSelectedCommentId] = useState(null);
   const [replyTo, setReplyTo] = useState("");
 
   const handleReply = (username) => {
     setReplyTo(`@${username}`);
+  };
+
+  useEffect(() => {
+    if (deleteCommentMutation.status === "success") {
+      toast.success("Comment deleted successfully");
+      deleteCommentMutation.reset();
+      setIsCommentModalOpen(false);
+      setSelectedCommentId(null);
+    } else if (deleteCommentMutation.status === "error") {
+      toast.error(deleteCommentMutation.error?.message || "Failed to delete comment");
+      deleteCommentMutation.reset();
+    }
+  }, [deleteCommentMutation.status, deleteCommentMutation.error]);
+
+  const handleDeleteComment = async () => {
+    if (!selectedCommentId || !post?.id) return;
+
+    await deleteCommentMutation.mutateAsync({
+      postId: post.id,
+      commentId: selectedCommentId,
+    });
   };
 
   if (!PostDetails) {
@@ -41,14 +61,11 @@ const PostDetails: React.FC<PostDetailsProps> = ({ isOpen, onClose, post }) => {
     <Modal isOpen={isOpen} onClose={onClose} className="w-[70%] bg-white">
       <section className="flex flex-col bg-black md:flex-row w-[100%] mx-auto max-w-[400px] md:max-w-[1500px] overflow-hidden border border-white">
         <PostHeader onOptionClick={() => setIsOptionsModalOpen(true)} className="flex md:hidden items-center justify-between p-2" />
-        {/* Image section */}
         <div className="w-full md:w-[1500px] aspect-square md:aspect-auto h-[200px] sm:h-[300px] md:h-[850px] bg-[#262626] flex items-center justify-center">
           {loadingPost ? <Spinner type="beat" /> : <img src={singlePost?.imageUrl} alt="Post" className="object-cover w-full h-full" />}
         </div>
-        {/* Right side: actions/comments */}
         <div className="w-full md:w-3/3 flex flex-col bg-black min-h-[200px] max-h-[30vh] md:min-h-[400px] md:max-h-[600px] p-2">
           <PostHeader onOptionClick={() => setIsOptionsModalOpen(true)} className="hidden md:flex items-center justify-between p-4" />
-          {/* Comments */}
           <div className="hidden md:flex md:flex-col md:min-h-[650px] md:max-h-[200px] overflow-y-auto px-4 p-2 border-t border-gray-700 pt-4">
             {loadingComments ? (
               <>
@@ -75,7 +92,13 @@ const PostDetails: React.FC<PostDetailsProps> = ({ isOpen, onClose, post }) => {
                         <Button className="text-xs text-gray-400" onClick={() => handleReply(c.username)}>
                           Reply
                         </Button>
-                        <Button className="text-xs text-gray-400" onClick={() => confirmDeleteComment(c.id)}>
+                        <Button
+                          className="text-xs text-gray-400"
+                          onClick={() => {
+                            setSelectedCommentId(c.id);
+                            setIsCommentModalOpen(true);
+                          }}
+                        >
                           <ICONS.ellipsis />
                         </Button>
                       </div>
@@ -93,11 +116,20 @@ const PostDetails: React.FC<PostDetailsProps> = ({ isOpen, onClose, post }) => {
               ))
             )}
           </div>
-          <CommentLikesFooter post={post} replyTo={replyTo} setReplyTo={setReplyTo} />
+          <CommentLikesFooter post={singlePost} replyTo={replyTo} setReplyTo={setReplyTo} />
         </div>
       </section>
       {/* Modals */}
-      <CommentModal isOpen={isCommentModalOpen} onClose={() => setIsCommentModalOpen(false)} />
+      <CommentModal
+        isOpen={isCommentModalOpen}
+        deleteCommentMutation={deleteCommentMutation.isPending}
+        onClose={() => {
+          setSelectedCommentId(null);
+          setIsCommentModalOpen(false);
+        }}
+        onDelete={handleDeleteComment}
+      />
+
       <PostOptionsModal
         post={post}
         isOpen={isOptionsModalOpen}
