2a07fa078c9cd2a40b44b387273068b62e437cdc
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-08-05 23:17:38 +0500

    add

--- src/components/postcomponents/CreatePost.tsx
+++ src/components/postcomponents/CreatePost.tsx
@@ -4,9 +4,10 @@ const CreatePost = ({ isOpen, onClose }) => {
   return (
     <>
       <Modal
+        title=""
         isOpen={isOpen}
         onClose={onClose} // handles outside click
-        className="w-[90%] sm:w-[500px] md:w-[600px] lg:w-[700px] xl:w-[800px] 2xl:w-[900px]"
+        className="w-[90%] sm:w-[500px] md:w-[600px] lg:w-[700px] xl:w-[800px] 2xl:w-[700px] "
       >
         <PostComponent isOpen={isOpen} onClose={onClose} />
       </Modal>
