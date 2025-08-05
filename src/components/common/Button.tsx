d339b29d5f63768dc90e2514455a06573d406f81
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-08-05 23:17:27 +0500

    add

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -4,12 +4,13 @@ interface ButtonProps {
   children: ReactNode;
   className?: string;
   type?: string;
+  disabled?: boolean;
   onClick?: () => void;
 }
 
-const Button: React.FC<ButtonProps> = ({ children, className = "", onClick }) => {
+const Button: React.FC<ButtonProps> = ({ children, className = "", onClick, disabled }) => {
   return (
-    <button className={`hover:cursor-pointer ${className}`} onClick={onClick}>
+    <button className={`hover:cursor-pointer ${className} ${disabled ? "opacity-50 cursor-not-allowed" : ""}`} onClick={onClick} disabled={disabled}>
       {children}
     </button>
   );
