c63da7f25cef5152115a0249d38cdc7f83cb993b
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-08-05 23:17:21 +0500

    add

--- src/components/comments/CommentModal.tsx
+++ src/components/comments/CommentModal.tsx
@@ -1,12 +1,11 @@
-// components/CommentModal.jsx
 import { Button, Modal } from "@components";
 
-const CommentModal = ({ isOpen, onClose, onDelete }) => {
+const CommentModal = ({ isOpen, onClose, onDelete, deleteCommentMutation }) => {
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <div className="bg-[#262626] p-4 rounded-md text-white w-96  flex flex-col space-y-4">
-        <Button className="text-red-500" onClick={onDelete}>
-          Delete
+        <Button className="text-red-500" onClick={onDelete} disabled={deleteCommentMutation}>
+          {deleteCommentMutation ? "Delete..." : "Delete"}
         </Button>
         <Button className="text-gray-300" onClick={onClose}>
           Cancel
