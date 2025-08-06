8374ec4c47b488449f1b543928cd8bd0ea1fb30e
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-08-06 18:32:36 +0500

    add

--- src/api/profileApi.ts
+++ src/api/profileApi.ts
@@ -1,5 +1,5 @@
-import { getUserProfile, uploadAvatar } from "@services";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { followUser, getAllUsers, getUserProfile, unfollowUser, uploadAvatar } from "@services";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 export const useUserProfile = (uid: string | undefined) => {
   return useQuery({
@@ -12,8 +12,35 @@ export const useUserProfile = (uid: string | undefined) => {
   });
 };
 
+export const useAllUsers = () => {
+  return useQuery({
+    queryKey: ["allUsers"],
+    queryFn: getAllUsers,
+  });
+};
+
 export const useUploadAvatar = () => {
   return useMutation({
     mutationFn: ({ file, userId }: { file: File; userId: string }) => uploadAvatar(file, userId),
+  });
+};
+
+export const useFollowUserMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: ({ currentUserId, targetUserId }: { currentUserId: string; targetUserId: string }) => followUser(currentUserId, targetUserId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["userProfile"] });
+    },
+  });
+};
+
+export const useUnfollowUserMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: ({ currentUserId, targetUserId }: { currentUserId: string; targetUserId: string }) => unfollowUser(currentUserId, targetUserId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["userProfile"] });
+    },
   });
 };
