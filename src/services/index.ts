5230c7e5a4de8d1c7345941dab330805e69a133b
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-08-06 18:34:50 +0500

    add

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,8 +1,10 @@
-export * as AuthService from "./authServices";
-export * as ProfileService from "./profileService";
+export * from "./authService";
+export * from "./profileService";
+export * from "./postService";
+// export * as ProfileService from "./profileService";
 // export * as FollowServices from "./followServices";
-export * as PostService from "./postService";
-export * as CommentService from "./commentService";
-export * as LikeService from "./likeService";
+// export * as PostService from "./postService";
+// export * as CommentService from "./commentService";
+// export * as LikeService from "./likeService";
 // export * as ProfileServices from "./profileServices";
 // export * as StoryServices from "./storyServices";
